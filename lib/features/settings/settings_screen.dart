358b560507101f87dd85ca2aea9e340bb3e4501d
pablo.briceno@clickonplans.com-Desarrollador <84730069+pablo-briceno-clickonplans@users.noreply.github.com>
2026-05-20 13:51:28 -0500

    Modify screen settings

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:kaku/shared/widgets/custom_app_bar.dart';
 
 class SettingsScreen extends StatelessWidget {
   const SettingsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('SettingsScreen')));
+    return Scaffold(
+      appBar: CustomAppBar(title: Text('Configuración'), defaultActions: false),
+      body: Center(child: Text('SettingsScreen')),
+    );
   }
 }
