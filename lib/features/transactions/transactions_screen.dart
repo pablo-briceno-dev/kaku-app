4100cc878849360573d7517fe0f1c17e1b538a64
pablo.briceno@clickonplans.com-Desarrollador <84730069+pablo-briceno-clickonplans@users.noreply.github.com>
2026-05-25 12:58:23 -0500

    Update file transactions_screen

--- lib/features/transactions/transactions_screen.dart
+++ lib/features/transactions/transactions_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:kaku/shared/widgets/custom_app_bar.dart';
 
 class TransactionsScreen extends StatelessWidget {
   const TransactionsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('TransactionsScreen')));
+    return Scaffold(
+      appBar: CustomAppBar(title: Text('Transacciones')),
+      body: Center(child: Text('TransactionsScreen')),
+    );
   }
 }
