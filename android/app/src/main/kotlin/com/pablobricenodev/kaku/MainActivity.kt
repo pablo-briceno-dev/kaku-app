8d2bfc9f50da151440fdda47d5b8b536b0715905
pablo.briceno@clickonplans.com-Desarrollador <84730069+pablo-briceno-clickonplans@users.noreply.github.com>
2026-06-01 16:06:45 -0500

    Update in MainActivity for security

--- android/app/src/main/kotlin/com/pablobricenodev/kaku/MainActivity.kt
+++ android/app/src/main/kotlin/com/pablobricenodev/kaku/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.pablobricenodev.kaku
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterFragmentActivity()
